363c99cc5e4580295dc9245096c85650e6c670da
Agunloye Olaniyi <nicolastfile@gmail.com>
2023-11-07 11:18:09 +0100

    Worked on implementing a basic authentication

---
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+"""
+implementation of Auth class
+"""
+
+from flask import request
+from typing import TypeVar, List
+from fnmatch import fnmatch
+User = TypeVar('User')
+
+
+class Auth:
+    """
+    Auth class template for all API authentication system
+    """
+
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """
+        returns True if the path is not in the list of strings excluded_paths
+        """
+        if path is None or excluded_paths is None or len(excluded_paths) == 0:
+            return True
+        slashed_path = path if path.endswith("/") else path + "/"
+        for pattern in excluded_paths:
+            if fnmatch(slashed_path, pattern):
+                return False
+        return True
+
+    def authorization_header(self, request=None) -> str:
+        """
+        return the value of the header request Authorization
+        """
+        auth_header = request.headers.get("Authorization")
+        if request is None or auth_header is None:
+            return None
+        return auth_header
+
+    def current_user(self, request=None) -> User:
+        """
+        returns None - request will be the Flask request object
+        """
+        return None
